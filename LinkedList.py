a9e93860da70520a76682ed8039cfa1f493d6a0a
NebularEclipse <143008010+NebularEclipse@users.noreply.github.com>
2023-08-30 11:51:43 +0800

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -23,13 +23,16 @@ class LinkedList:
 		print("Are you sure you want to exit?")
 		print("[0] Yes")
 		print("[1] No")
-		
-		response = int(input())
-		
+
+		response = int(input("Enter option: "))
+
 		if response == 0:
 			self.head = None
+			clear()
+			print("Thank you!")
 			exit()
 		elif response == 1:
+			clear()
 			return
 		else:
 			clear()
@@ -43,7 +46,7 @@ class LinkedList:
 
 	def insert_end(self, data):
 		new_node = Node(data)
-		
+
 		if self.head is None:
 			self.head = new_node
 		else:
@@ -56,7 +59,7 @@ class LinkedList:
 		if position == 1:
 			self.insert_start(data)
 			return
-			
+
 		new = Node(data)
 		temp = self.head
 		for i in range(2, position):
@@ -86,7 +89,7 @@ class LinkedList:
 		if position == 1 and self.head is not None:
 			self.delete_start()
 			return
-			
+
 		temp = self.head
 		for i in range(2, position):
 			temp = temp.next
@@ -101,14 +104,14 @@ class LinkedList:
 			self.delete_start()
 			print(f"{target} has been successfully deleted.")
 			return
-			
+
 		temp = self.head
 		while temp.next is not None:
 			if temp.next.data == target:
 				temp.next = temp.next.next
 				print(f"{target} has been successfully deleted.")
 			temp = temp.next
-		
+
 		print(f"{target} is not on the list and cannot be deleted.")
 
 	def find(self, target):
@@ -140,3 +143,50 @@ class LinkedList:
 			print(temp.data, end=" -> ")
 			temp = temp.next
 		print("None")
+		print()
+		
+def main():
+	list = LinkedList()
+	clear()
+	while True:
+		print("[0]  Exit")
+		print("[1]  Insert At Start")
+		print("[2]  Insert At End")
+		print("[3]  Insert At Position")
+		print("[4]  Delete At Start")
+		print("[5]  Delete At End")
+		print("[6]  Delete At Position")
+		print("[7]  Delete Number")
+		print("[8]  Search Number")
+		print("[9]  Display Number At Position")
+		print("[10] Display List")
+		i = int(input("Enter option: "))
+		clear()
+		if i == 0:
+			list.terminate()
+		elif i == 1:
+			list.insert_start(int(input("Enter number: ")))
+		elif i == 2:
+			list.insert_end(int(input("Enter number: ")))
+		elif i == 3:
+			list.insert_at(int(input("Enter number: ")), int(input("Enter position: ")))
+		elif i == 4:
+			list.delete_start()
+		elif i == 5:
+			list.delete_end()
+		elif i == 6:
+			list.delete_at(int(input("Enter position: ")))
+		elif i == 7:
+			list.delete_value(int(input("Enter value: ")))
+		elif i == 8:
+			list.find(int(input("Enter value: ")))
+		elif i == 9:
+			list.display_at(int(input("Enter position: ")))
+		elif i == 10:
+			list.display()
+		else:
+			main()
+		
+
+if __name__ == "__main__":
+	main()
